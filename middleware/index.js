554c05454e2c69e2a611c8ff0da6bed58a667234
whamsy <waatluri@gmail.com>
2017-05-01 15:14:24 -0400

    changed session parameters being used

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,11 @@
 function loggedOut(req, res, next) {
-    if (req.session && req.session.userId) {
+    if (req.session && req.session.username) {
         return res.redirect('/profile');
     }
     return next();
 }
 function requiresLogin(req, res, next) {
-    if (req.session && req.session.userId) {
+    if (req.session && req.session.username) {
         return next();
     } else {
         var err = new Error('You must be logged in to view this page.');
