2fe9e46bf76a064f570bbfdaa86623769a4e2a0b
Varun Chopra <vc1202@nyu.edu>
2017-05-03 22:01:54 -0400

    Added getuserdetailsfunction

--- functions.js
+++ functions.js
@@ -2,12 +2,16 @@
  * Created by jaildar on 02/05/17.
  */
 
+
 var AWS1 = require("aws-sdk");
 
 AWS1.config.update({
     region: "us-west-2",
     endpoint: "http://localhost:3000"
 });
+
+
+
 
 function createtables(tablename,primarykeyname,primarykeytype) //"KeyType should be "N","S" or "B" where N is number,S is string and B is binary
 {
@@ -45,7 +49,7 @@ function createtables(tablename,primarykeyname,primarykeytype) //"KeyType should
 function adduser(UserID,Name,Preference,ProfilePictureLink) //Preference should be in form of array []
 {
     var documentClient = new AWS1.DynamoDB.DocumentClient();
-    var table = "USER";
+    var table = "USERS";
 
 
 
@@ -81,6 +85,65 @@ function adduser(UserID,Name,Preference,ProfilePictureLink) //Preference should 
 
 }
 
+
+function getuserdetails(username,callback) {
+
+    var docClient = new AWS1.DynamoDB.DocumentClient();
+    var table = "USER";
+
+
+    var params = {
+        TableName: table,
+        Key: {
+            "UserID": username  //Username for which data is needed
+        }
+    };
+
+
+    docClient.get(params,function (err, data) {
+
+
+        if (err) {
+
+            return callback(err);
+            // console.error("Unable to read item. Error JSON:", err); //if(err) impies there is no data, so it will return null
+            // value1=err; // no need for this
+            // return value1;
+            // console.log(err) //will show you the error or callback(err) will pass this to the callback
+
+        } else{
+            var value1={};
+            value1["Name"]=data.Item.Name;
+             value1["Rating"]=data.Item.Rating;
+            value1["ProfilePic"]=data.Item.Profile_Picture;
+            value1["Room_Interested"]=data.Item.Room_Interested;
+            value1["Preferences"]=data.Item.Preferences;
+            value1["User_ID"]=data.Item.UserID;
+
+            return(callback(null,value1));
+
+        }});
+
+}
+
+
+
+//var x=getuserdetails(2,function (error, result){
+
+  //  if (error || !result) {
+
+    //    console.log(error);
+    //}  else {
+      //  console.log(result);
+    //}
+
+//});
+
+
+
+
+
+
 function addroom(RoomId,Name,UserID,Address,NoofRooms,PetFriendly,Neighbourhood,Utilities,Rent)
 {
 
@@ -115,11 +178,14 @@ function addroom(RoomId,Name,UserID,Address,NoofRooms,PetFriendly,Neighbourhood,
         if (err) {
             console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("Added item:", JSON.stringify(data, null, 2));
+
+            console.log(data);
         }
     });
 
 }
+
+
 
 
 function addTasktoRoom(TaskID,RoomID)
